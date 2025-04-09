bd8704191a1121bd56edbec573df5afdccc47454
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-09 23:20:18 +0300

    commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,5 @@
 // src/data/data.js
 
-export const name = "Dennis";
+export const name = "Dennis Muasya Ngui";
 export const city = "Nairobi";
 export const image ="https://www.google.com/imgres?q=car&imgurl=https%3A%2F%2Fstatic.independent.co.uk%2F2025%2F02%2F18%2F10%2F40%2FKia-EV6.png&imgrefurl=https%3A%2F%2Fwww.independent.co.uk%2Fcars%2Felectric-vehicles%2Felectric-cars-vs-petrol-diesel-should-buy-b2625959.html&docid=qxMPZKUcEd4RtM&tbnid=eTVbE6jd-hVx7M&vet=12ahUKEwjhj8a34suMAxXKZ0EAHcBrDI0QM3oECB0QAA..i&w=2048&h=1536&hcb=2&ved=2ahUKEwjhj8a34suMAxXKZ0EAHcBrDI0QM3oECB0QAA";
