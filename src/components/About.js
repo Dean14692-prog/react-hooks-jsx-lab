b02086c9fcc29d3758ba7cbe652112061bdde0b6
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-09 20:33:37 +0300

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js"; // Ensure this image variable points to a valid image file
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I enjoy building websites and learning JavaScript!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,6 @@
-// use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const name = "Dennis Muasya Ngui";
+const city = "Nairobi"; 
+const image = "https://via.placeholder.com/150"; 
 
 export { name, city, image };
